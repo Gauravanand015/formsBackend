7372fdfac717d2165317e640b472ada7997febba
Gaurav Anand <gauravanand67584@gmail.com>
2023-07-27 00:32:16 +0530

    Backend for deployment

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 
-const connection = mongoose.connect(process.env.mongoUrl, {
+const connection = mongoose.connect(process.env.MongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
